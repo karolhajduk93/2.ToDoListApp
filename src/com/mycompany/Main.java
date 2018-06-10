36eace0c4e2fc37e05fe9f3bdda778248d052560
Karol <karolhajduk93@gmail.com>
2018-06-10 18:57:58 +0200

    Display task after adding

--- src/com/mycompany/Main.java
+++ src/com/mycompany/Main.java
@@ -1,9 +1,15 @@
 package com.mycompany;
 
 import javax.swing.*;
+import javax.swing.border.BevelBorder;
+import javax.swing.border.EtchedBorder;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 
@@ -11,50 +17,90 @@ import static java.awt.Font.PLAIN;
 
 public class Main extends JFrame{
 
+    JSpinner dateSpin;
+    Box eventBox, taskBox, boxBox;
+    JTextField eventName;
+    Date date;
+    Checkbox doneCheck;
+    JLabel taskLabel;
+    JPanel panel;
+
     public static void main(String[] args) {
 
         new Main();
     }
 
     private Main(){
-        this.setSize(400, 400);
+        this.setSize(400, 600);
         this.setTitle("To-Do List");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setResizable(false);
         this.setLocationRelativeTo(null);
 
-        //miejsce na wpisanie nazwy i podanie daty
-        //klikamy date - data zapisana.
-        // po dodaniu (kliknieciu enter) size != null data do dzisiejszej i dodajemy zadanie (instancje klasy)
-        //dodajemy gwiazdke czy oznaczac jako wazne
+        panel = new JPanel();
+        boxBox = Box.createVerticalBox();
 
-        JPanel panel = new JPanel(new BorderLayout());
-
-        Box eventBox = Box.createHorizontalBox();
+        eventBox = Box.createHorizontalBox();
+        eventBox.setBorder(BorderFactory.createEtchedBorder());
 
         JLabel plusSign = new JLabel("+");
-        plusSign.setFont(new Font("Serafi", PLAIN, 40));
+        plusSign.setFont(new Font("Serafi", PLAIN, 20));
         eventBox.add(plusSign);
 
-        JTextArea jTextArea = new JTextArea();
-        panel.add(jTextArea, BorderLayout.CENTER);
 
-        JTextField event = new JTextField(30);
-        event.setPreferredSize(new Dimension(40,30));
-        event.setFont(new Font("Times", Font.ITALIC, 20));
-        eventBox.add(event);
 
-        ImageIcon calendar = new ImageIcon("seo-social-web-network-internet_104-128.png");
-        Image image = calendar.getImage();
-        Image newImage = image.getScaledInstance(40, 40, Image.SCALE_SMOOTH);
-        calendar = new ImageIcon(newImage);
+        eventName = new JTextField(20);
+        eventName.setFont(new Font("Times", Font.ITALIC, 15));
+        eventName.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(e.getSource() == eventName){
+                    /* to do: saving every task in some file, figure out how to remove
+                       those tasks (checkbox + jlabel) -> whole box from screen or how to send them to "done"
+                       aslo "done" to do*/
 
-        JButton date = new JButton(calendar);
-        date.setBorder(BorderFactory.createEmptyBorder());
-        date.setContentAreaFilled(false);
-        date.setPreferredSize(new Dimension(40, 40));
-        eventBox.add(date);
-        panel.add(eventBox, BorderLayout.NORTH);
+                    SimpleDateFormat simpleDate = new SimpleDateFormat("E-dd-MM-yyyy");
+                    taskBox = Box.createHorizontalBox();
+                    taskBox.setBorder(BorderFactory.createTitledBorder(simpleDate.format(date)));
 
+                    doneCheck = new Checkbox();
+                    panel.add(doneCheck);
+                    taskBox.add(doneCheck);
+
+                    String taskName = eventName.getText();
+                    taskLabel = new JLabel(taskName);
+                    taskLabel.setMinimumSize(new Dimension(500,10));
+                    taskLabel.setPreferredSize(new Dimension(300, 10));
+                    taskBox.add(taskLabel);
+
+                    boxBox.add(taskBox);
+                    
+                    panel.add(boxBox);
+                    panel.revalidate();
+                    panel.repaint();
+
+                }
+            }
+        });
+        eventBox.add(eventName);
+
+        date = new Date();
+        dateSpin = new JSpinner(new SpinnerDateModel(date, null, null, Calendar.DAY_OF_MONTH));
+        JSpinner.DateEditor dateEditor = new JSpinner.DateEditor(dateSpin, "dd/MM/yy");
+        dateSpin.setEditor(dateEditor);
+        dateSpin.addChangeListener(new ChangeListener() {
+            @Override
+            public void stateChanged(ChangeEvent e) {
+                if(e.getSource() == dateSpin){
+                    date = (Date) dateSpin.getValue();
+
+                }
+            }
+        });
+        eventBox.add(dateSpin);
+
+        panel.add(eventBox, new FlowLayout());
+        
         this.add(panel);
         this.setVisible(true);
     }
